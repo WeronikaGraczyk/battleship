6b4eb1d125f53b74c6428e0bf5c9a0a6b4fe2a28
Weronika Graczyk <wercia01@onet.pl>
2022-01-03 20:03:20 +0100

    add Point

---
+++ src/Point.java
@@ -0,0 +1,27 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(String point) {
+        this.x = whatsSecondNumber(point);
+        this.y = whatsFirstNumber(point);
+    }
+
+    private int whatsFirstNumber(String place) {
+        String i = place.substring(0);
+        return i.charAt(0) - 64;
+    }
+
+    private int whatsSecondNumber(String place) {
+        String j = place.substring(1);
+        return Integer.parseInt(j);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
